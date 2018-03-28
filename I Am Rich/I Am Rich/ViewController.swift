17c29c2fd7baff8bbb8ef4cfda35439656ade0d4
JoaquinHervasFarru <joaquinhervasfarru@gmail.com>
2018-03-27 19:35:11 -0500

    First commit, project created

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Joaquín Hervas Farrú on 3/27/18.
+//  Copyright © 2018 Joaquín Hervas Farrú. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
